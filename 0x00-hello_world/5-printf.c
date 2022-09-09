b58f592871e44633048e4823358bf99ed9a4224a
Eniola <Eniolaakinsola007@gmail.com>
2022-09-08 19:22:08 -0700

    5 print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main-entry point for the program
- *
- * Return: zero if no errors, return non zero if errors
+ *main-entry point for the program
+ *Return: zero if no errors, return non zero if errors
  */
 int main(void)
 {
