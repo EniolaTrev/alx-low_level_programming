e04c420ee7583d4ce19dd9f7b60ed9122cd2b66a
Eniola <Eniolaakinsola007@gmail.com>
2022-09-09 18:56:31 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
@@ -10,6 +11,7 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
@@ -29,4 +31,5 @@ printf("%i is positive\n", n);
 }
 
 return (0);
+
 }
